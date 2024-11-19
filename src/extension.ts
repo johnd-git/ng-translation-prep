6a532ac14994fae7edab3df12fdc1d9e6d6599bc
johnd <j-devine1@outlook.com>
2024-11-19 10:08:44 -0500

    get editor in extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,28 +5,41 @@ import * as vscode from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "i18n-lint" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "i18n-lint.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from i18n-lint!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
+  // Replace the i18nPlainTextLint command implementation
   const i18nPlainTextLint = vscode.commands.registerCommand(
     "i18n-lint.i18nPlainTextLint",
-    () => {
-      vscode.window.showInformationMessage("Hello from i18nPlainTextLint!");
+    async () => {
+      vscode.window.showInformationMessage("Hello World from i18n-lint!");
+
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showWarningMessage("No active editor found");
+        return;
+      }
+
+      // Check if it's an HTML file
+      if (!editor.document.fileName.endsWith(".html")) {
+        vscode.window.showWarningMessage(
+          "This command only works with HTML files"
+        );
+        return;
+      }
+
+      const text = editor.document.getText();
+      const diagnostics: vscode.Diagnostic[] = [];
+
+      // Create diagnostics collection
+      const diagnosticCollection =
+        vscode.languages.createDiagnosticCollection("i18n-lint");
+
+      // TODO: Implement HTML parsing and checking logic
+      // For now, let's just show we've processed the file
+      vscode.window.showInformationMessage(
+        `Checking file: ${editor.document.fileName}`
+      );
     }
   );
 
